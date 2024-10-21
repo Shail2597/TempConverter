8600ca05bd97065e4789ec7a5aa39a5aeb272035
Shail2597 <shail.ch259@gmail.com>
2024-10-21 15:05:22 -0600

    Update Temp-Converter-Streamlit.py

--- Temp-Converter-Streamlit.py
+++ Temp-Converter-Streamlit.py
@@ -1,4 +1,4 @@
 import streamlit as st
-st.print("Press 1 if you want to Convert from Celsius")
-st.print("Press 2 if you want to Convert from Farenheit")
-st.print("Press 3 if you want to Convert from Kelvin")
+st.write("Press 1 if you want to Convert from Celsius")
+st.write("Press 2 if you want to Convert from Farenheit")
+st.write("Press 3 if you want to Convert from Kelvin")
